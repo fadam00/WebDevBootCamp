14a836cc65e5b8bc1c937cd14b9a4d588a9d9ed3
Frank Adames <adamesfra00@gmail.com>
2020-04-16 14:46:05 -0400

    completed while loop exercise

--- Javascript/whileloopexercise.js
+++ Javascript/whileloopexercise.js
@@ -4,6 +4,10 @@
 
 var num1;
 var num2;
+var num3;
+var num4;
+var num5;
+var num6;
 
 //While Loop
 
@@ -11,7 +15,7 @@ num1 = 1;
 
 while (num1 <= 10) {
 
-    console.log(num);
+    console.log(num1);
     num1 += 2;
 }
 
@@ -23,5 +27,36 @@ while (num2 <= 20) {
     }
 
     num2++;
+}
+
+num3 = -10;
+
+while (num3 <= 19){
+    console.log(num3);
+    num3++;
+}
+
+num4 = 10;
+while (num6 <= 40){
+    if (num4 % 2 === 0){
+    console.log(num4);
+    }
+    num4++;
+}
+
+num5 = 300;
+while (num5 <= 333){
+    if(num5 % 2 != 0){
+    console.log(num5);
+    }
+    num5++;
+}
+
+num6 = 5;
+while (num6 <= 50){
+    if (num6 % 3 === 0 && num6 % 5 === 0){
+    console.log(num6);
+    }
+    num6++;
 }
 
