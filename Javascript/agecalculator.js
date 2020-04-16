260d72c580db4a6f9833104247b32f2ef1997fed
Frank Adames <adamesfra00@gmail.com>
2020-04-16 14:03:41 -0400

    added age calculator exercise

---
+++ Javascript/agecalculator.js
@@ -0,0 +1,15 @@
+// Age Calculator - See how many days you've been alive!
+
+//Variables
+
+var currentAge;
+var daysAlive;
+
+//Calculator
+
+currentAge = prompt("How old are you?");
+daysAlive = currentAge * 365;
+
+//Alerts + Console.log
+
+alert("You've been alive for " + daysAlive + "days .");
