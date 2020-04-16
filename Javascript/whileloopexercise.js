c9f15878097814aad3ce098d17a301166d7ec0d0
Frank Adames <adamesfra00@gmail.com>
2020-04-16 14:35:33 -0400

    added while loops exercises

---
+++ Javascript/whileloopexercise.js
@@ -0,0 +1,27 @@
+//While Loops Exercise
+
+//Variables
+
+var num1;
+var num2;
+
+//While Loop
+
+num1 = 1;
+
+while (num1 <= 10) {
+
+    console.log(num);
+    num1 += 2;
+}
+
+num2 = 1;
+
+while (num2 <= 20) {
+    if(num2 % 4 === 0){
+        console.log(num2);
+    }
+
+    num2++;
+}
+
