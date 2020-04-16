e6f57168cc4192dbecd375b0bf7a0156b3666434
Frank Adames <adamesfra00@gmail.com>
2020-04-16 13:56:45 -0400

    added js stalker exercise

---
+++ Javascript/jsstalkerexercise.js
@@ -0,0 +1,19 @@
+//Exercise that uses Variables, Strings, Prompts, and Console.log
+
+//Variables
+var firstName;
+var lastName;
+var age;
+
+//Prompts
+
+firstName = prompt("What is your first name?");
+
+lastName = prompt("What is your last name?");
+
+age = prompt("How old are you?");
+
+//Console.log
+
+console.log("Hi there, " + firstName + " " + lastName + ". Nice to meet you.");
+console.log ("I see you're " + age + " years old.");
