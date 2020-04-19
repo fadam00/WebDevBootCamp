2746f2dcdc8a5dab7ff5dc39dcfa0bc34a4b9bf1
Frank Adames <adamesfra00@gmail.com>
2020-04-19 13:49:52 -0400

    todo list improvements

--- Javascript/todolist.js
+++ Javascript/todolist.js
@@ -5,22 +5,43 @@ window.setTimeout(function(){
 
     while (input !== "quit"){
 
-        //handle input
-        if(input === "list"){
-            console.log(todos);
-        } else if (input === "new"){
-        //ask for new task
-            var newTask = prompt("Enter new task");
-        //add task to array
-            todos.push(newTask);
-        }
+            if(input === "list"){
+                listTasks();
 
-        //ask again for new input
+            } else if (input === "new"){
+                addTask();
+ 
+            } else if (input === "delete"){
+                deleteTask();
+            }
+    
         input = prompt("What would you like to do?");
 
     }
 
     console.log("Ok, you quit the App");
 
+    function listTasks(){
 
-}, 500);
+        console.log("**************")
+        todos.forEach(function(todo, i){ //1st argument is the item in the array, 2nd arguments is the index of the item
+            console.log(i + ": " + todo);
+        });
+        console.log("**************");
+
+    }
+
+    function addTask(){
+        var newTask = prompt("Enter new task");
+        todos.push(newTask);
+        console.log("Added task");
+    }
+
+    function deleteTask(){
+        var index = prompt("Enter index of task to delete");
+        todos.splice(index,1) //1st argument what index you'd like to start deleting from. 2nd argument how many items to delete after index entered.
+        console.log("Deleted task");
+    }
+
+
+}, 1000);
