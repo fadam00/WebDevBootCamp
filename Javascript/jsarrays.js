033ee5b8e396299339fb5790513e8c90f19c9a53
Frank Adames <adamesfra00@gmail.com>
2020-04-19 11:20:46 -0400

    js array methods

---
+++ Javascript/jsarrays.js
@@ -0,0 +1,35 @@
+//Array Methods
+
+//Push and Pop
+//Use Push to ADD an element to the end of the array
+
+color = [];
+color.push("indigo");
+
+// Use Pop to remove the last element of the array
+
+color = ["indigo", "red"];
+color.pop();
+//removes "red";
+
+//Shift and Unshift
+//Same as push and pop but to the first item of the array
+color.unshift("infrared"); // Adds the item
+color.shift(); // Removes first element.
+
+//IndexOf finds the index of an item in an array
+
+friends = ["Mattias", "Julie", "Ralph"];
+friends.IndexOf("Mattias");
+//Returns -1 if the element is NOT present.
+
+//Slice copies parts of an array
+//Takes 2 arguments which represent where to START the cut and where the END the cut
+
+fruits = ["strawberry", "mangoes", "orange", "lemon"];
+
+var citrus = fruits.slice(3,4);
+
+
+
+
