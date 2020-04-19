793d0e8bbef82f94af8222c0d9764d376153ba6a
Frank Adames <adamesfra00@gmail.com>
2020-04-19 10:49:53 -0400

    js scope and higher order functions

---
+++ Javascript/jsscope.js
@@ -0,0 +1,14 @@
+// Scope -- the context that code is executed in
+//Higher Order Functions -- Pass a function to other function!
+
+function sing(){
+    console.log("Twinkle, Twinkle");
+    console.log("Little star");
+}
+
+setInterval(sing,1000);
+
+//Anonymous Function - you can declare and call a function within a function
+
+
+ 
