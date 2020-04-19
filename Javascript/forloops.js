6916316bd6ae225d37e266345ddc951b0a3935c0
Frank Adames <adamesfra00@gmail.com>
2020-04-19 09:41:06 -0400

    forloops

---
+++ Javascript/forloops.js
@@ -0,0 +1,17 @@
+// For loops 
+// syntax - for(var count = 0; count < 6; count++){ console.log(count)};
+// Exercises
+
+for(i = -10; i < 20; i++){
+    console.log(i);
+}
+
+for(i = 10; i < 40; i+= 2){
+    console.log(i)
+}
+
+for(i = 300; i < 333; i++){
+    if (i % 2 != 0){
+        console.log(i);
+    }
+}
