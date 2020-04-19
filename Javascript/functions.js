bfcbc85bd8fe2f2f7a91a020d559f0e730bc10b5
Frank Adames <adamesfra00@gmail.com>
2020-04-19 09:49:16 -0400

    functions example

---
+++ Javascript/functions.js
@@ -0,0 +1,9 @@
+//Functions are reusabe chunks of code (20 line limit) in Javascript
+//Example
+
+function singASong(){
+    console.log("Twinkle, twinkle little star");
+    console.log("Something, something that you are");
+}
+
+singASong();
